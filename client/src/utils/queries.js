603dc8853df95865c8c7e04715356258afed8467
Travis Helms <smasher.coder@gmail.com>
2022-02-21 13:33:40 -0600

    fixed token problem

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,7 +6,7 @@ export const GET_ME = gql`
         _id
         username
         email
-        bookcount
+        bookCount
         savedBooks{
             bookId
             authors
@@ -16,4 +16,5 @@ export const GET_ME = gql`
             link
         }
     }
-}`;
+}
+`;
